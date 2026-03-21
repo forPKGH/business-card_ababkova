e1a627935643a0f8df9661b130e7ddc42538f134
Your Name <you@example.com>
2026-03-21 14:30:54 +0300

    fix: contacts block updated with correct arrangement refactor: strings imported as variables

--- app/src/main/java/com/example/mycoolbusinesscard/MainActivity.kt
+++ app/src/main/java/com/example/mycoolbusinesscard/MainActivity.kt
@@ -11,15 +11,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mycoolbusinesscard.ui.theme.MyCoolBusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,22 +51,31 @@ fun BusinessCard(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.padding(8.dp)
     ) {
-        Column (
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Photo(img = painterResource(R.drawable.business_card_photo))
-            Title(text = "Квентин Тарантино")
-            Subtitle(text = "кинорежиссёр и сценарист")
-        }
+        MainInfoBlock()
+        ContactsBlock()
+    }
+}
 
-        Column (
-            verticalArrangement = Arrangement.spacedBy(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            IconTextRow(text = "+7 (800) 555-35-35", icon = painterResource(R.drawable.phone))
-            IconTextRow(text = "+7 (999) 555-36-36", icon = painterResource(R.drawable.max))
-            IconTextRow(text = "vistatheatrehollywood@gmail.com", icon = painterResource(R.drawable.email))
-        }
+@Composable
+fun MainInfoBlock(modifier: Modifier = Modifier) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Photo(img = painterResource(R.drawable.business_card_photo), modifier = Modifier.width(200.dp))
+        Title(text = stringResource(R.string.worker_name), fontSize = 30.sp)
+        Subtitle(text = stringResource(R.string.job))
+    }
+}
+
+@Composable
+fun ContactsBlock(modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.spacedBy(24.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        IconTextRow(text = stringResource(R.string.phone_number), icon = painterResource(R.drawable.phone))
+        IconTextRow(text = stringResource(R.string.social_media), icon = painterResource(R.drawable.max))
+        IconTextRow(text = stringResource(R.string.email), icon = painterResource(R.drawable.email))
     }
 }
 
@@ -68,41 +83,47 @@ fun BusinessCard(modifier: Modifier = Modifier) {
 fun Photo(img: Painter, modifier: Modifier = Modifier) {
     Image(
         painter = img,
-        contentDescription = "Квентин Тарантино",
-        modifier = modifier
+        contentDescription = stringResource(R.string.worker_name),
+        modifier = modifier,
+        contentScale = ContentScale.Crop
     )
 }
 
 @Composable
-fun Title(text: String, modifier: Modifier = Modifier) {
+fun Title(text: String, modifier: Modifier = Modifier, fontSize: TextUnit = 32.sp, fontWeight: FontWeight = FontWeight.Black) {
     Text(
         text = text,
-        modifier = modifier
+        fontSize = fontSize,
+        modifier = modifier,
+        fontWeight = fontWeight
     )
 }
 
 @Composable
-fun Subtitle(text: String, modifier: Modifier = Modifier) {
+fun Subtitle(text: String, modifier: Modifier = Modifier, fontSize: TextUnit = 16.sp, fontWeight: FontWeight = FontWeight.SemiBold) {
     Text(
         text = text,
-        modifier = modifier
+        fontSize = fontSize,
+        modifier = modifier,
+        fontWeight = fontWeight
     )
 }
 
 @Composable
-fun IconTextRow(text: String, icon: Painter, modifier: Modifier = Modifier) {
+fun IconTextRow(text: String, icon: Painter, modifier: Modifier = Modifier, fontSize: TextUnit = 16.sp, contentDescription:String? = null) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = modifier.height(48.dp)
+        modifier = modifier.height(24.dp)
     ) {
         Image(
             painter = icon,
-            contentDescription = "phone",
-            modifier = modifier
+            contentDescription = contentDescription,
+            modifier = Modifier.width(24.dp)
         )
         Text(
             text = text,
+            fontSize = fontSize,
             modifier = modifier
         )
     }
